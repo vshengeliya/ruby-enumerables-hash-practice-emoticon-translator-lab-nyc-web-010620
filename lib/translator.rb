698178e85143943faa2a69bfce74ab84c984f220
bright-format-7059 <v.shengeliya@gmail.com>
2019-12-18 01:29:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,23 +1,19 @@
-# require modules here
 require "yaml"
 
-emoticons
-def load_library(emoticons)
- emoticons = YAML.load_file('emoticons.yml')
-end
-
-def get_japanese_emoticon
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  
   new_hash = {
-    'get_meaning' => {}
+    'get_meaning' => {},
     'get_emoticon' => {}
   }
-  emoticons.each do |key, value|
-      new_hash['get_emoticon'][value[0]] = emoticons[key][1]
-      new_hash['get_meaning'][value[1]] = key
+
+  emoticons.each do |key,value|
+    new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+    new_hash['get_meaning'][value[1]] = key
   end
 
   new_hash
-    
 end
 
 def get_japanese_emoticon(file_path, emoticon)
